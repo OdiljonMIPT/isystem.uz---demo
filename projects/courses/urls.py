ee0d157cc8ee693b5ed1106e9474a58db1effc41
odiljonmipt <odiljonabduvaitov@gmail.com>
2022-07-02 19:04:16 +0500

    updated

---
+++ projects/courses/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', home),
+    path('about/', about),
+    path('contact/', contact),
+    path('courses/', courses_view),
+    path('courses/<slug:slug>', courses_detail_view),
+    # path('add/<str:a>/<str:b>', add_view),
+]
